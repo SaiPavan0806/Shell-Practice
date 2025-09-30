ceb4bc3ab10a66f1129136ba26925cf026030ea1
Sai Pavan Bharath (LTIMindtree Limited) <v-sabharath@microsoft.com>
2025-09-29 21:33:44 -0700

    some message

--- 03-variables.sh
+++ 03-variables.sh
@@ -5,7 +5,7 @@ PERSON1=John
 PERSON2=Mike
 
 
-echo "$PERSON1:: Mike, How're you doing"
+echo "$PERSON1:: $PERSON2, How're you doing"
 echo "$PERSON2:: Hi $PERSON1, I'm doing great. How are you ?"
 echo "$PERSON1:: I'm doing great as well, It's been ages where we met. It's really lovely to see you"
 echo "$PERSON2:: Likewise $PERSON1"

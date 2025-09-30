bd1222a620bff5624ad379d8507fe5e97d8a198a
Sai Pavan Bharath (LTIMindtree Limited) <v-sabharath@microsoft.com>
2025-09-29 21:26:16 -0700

    some message

--- 02. conversations.sh
+++ 02. conversations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "John: Mike, How're you doing"
-echo "Mike: Hi John, I'm doing great. How are you ?"
-echo "John: I'm doing great as well, It's been ages where we met. It's really lovely to see you"
-echo "Likewise John"
+echo "John:: Mike, How're you doing"
+echo "Mike:: Hi John, I'm doing great. How are you ?"
+echo "John:: I'm doing great as well, It's been ages where we met. It's really lovely to see you"
+echo "Mike:: Likewise John"

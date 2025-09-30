d480c773da7611f0a88ef9712de8370c7a0c0920
Sai Pavan Bharath (LTIMindtree Limited) <v-sabharath@microsoft.com>
2025-09-29 22:01:07 -0700

    some message

--- 05. variables3.sh
+++ 05. variables3.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# read command in shell helps us to read the file/text with privacy
-
 echo "please enter your PIN number"
-read -s PIN_NUMBER # here PIN_NUMBER is variable
+read -s PIN_NUMBER # here PIN_NUMBER is variable & "-s" makes sure the text what we're typing is hidden
 
 echo "PIN entered is: $PIN_NUMBER"

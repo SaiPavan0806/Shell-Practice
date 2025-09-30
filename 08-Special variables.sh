ffad202ae31ee226c047aeb31eae21711e3ac0cf
Sai Pavan Bharath (LTIMindtree Limited) <v-sabharath@microsoft.com>
2025-09-29 22:38:33 -0700

    some message

--- 08-Special variables.sh
+++ 08-Special variables.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-echo "All variables passed to the script: $@" # @ is used to print to all the arguments
-echo "All variables passed to the script: $*" # *is used to
+# @&* is used to print to all the arguments
+echo "All variables passed to the script: $@"
+echo "All variables passed to the script: $*" 
+echo "Current directory: $PWD"
+echo "Who is running the scripr: $USER"
+echo "home directory of the user: $HOME"
+echo "show the process instance ID: $$"

d381ef33c3dbc298fcdedfb43ca3841f3309c1e8
Sai Pavan Bharath (LTIMindtree Limited) <v-sabharath@microsoft.com>
2025-09-30 16:17:20 -0700

    message

---
+++ 15. Installation-colors.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+R="\e[31m" #Red
+G="\e[32m" #Green
+Y="\e[33m" #Yellow
+N="\e[0m" #white/Normal color we can use 37 as well in place of 0
+
+echo -e "$G Hello World"
+echo "check this color"

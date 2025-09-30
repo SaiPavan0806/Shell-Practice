8fd428ad0c7284bfe22695006aaf5eeee8bb3fea
Sai Pavan Bharath (LTIMindtree Limited) <v-sabharath@microsoft.com>
2025-09-29 21:32:46 -0700

    some message

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# No space before or after equal
+PERSON1=John
+PERSON2=Mike
+
+
+echo "$PERSON1:: Mike, How're you doing"
+echo "$PERSON2:: Hi $PERSON1, I'm doing great. How are you ?"
+echo "$PERSON1:: I'm doing great as well, It's been ages where we met. It's really lovely to see you"
+echo "$PERSON2:: Likewise $PERSON1"

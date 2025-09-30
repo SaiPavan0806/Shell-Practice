4c89a3b430775c8ab02d64e36915fcc60501e8c2
Sai Pavan Bharath (LTIMindtree Limited) <v-sabharath@microsoft.com>
2025-09-29 21:59:47 -0700

    some message

--- 05. variables3.sh
+++ 05. variables3.sh
@@ -3,6 +3,6 @@
 # read command in shell helps us to read the file/text with privacy
 
 echo "please enter your PIN number"
-read PIN_NUMBER # here PIN_NUMBER is variable
+read -s PIN_NUMBER # here PIN_NUMBER is variable
 
 echo "PIN entered is: $PIN_NUMBER"

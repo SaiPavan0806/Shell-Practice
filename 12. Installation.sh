dc5617ffaccbe9b19878ae7a5b9eb360aee21fd5
Sai Pavan Bharath (LTIMindtree Limited) <v-sabharath@microsoft.com>
2025-09-30 15:35:29 -0700

    message

--- 12. Installation.sh
+++ 12. Installation.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR: Installing MySQL is FAILURE"
+    exit 1
 else
     echo "Installing MySQl is SUCCESS"
 fi
